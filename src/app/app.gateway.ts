021f9d8725a64a7e6305881a560c5144ce33f52e
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-06-06 02:10:38 +0300

    Create methods for chat

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,9 +1,36 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { AppService } from '../app.service';
+import { Prisma } from '@prisma/client';
 
 @WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private appService: AppService) {}
+
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(client: Socket, payload: Prisma.ChatCreateInput): Promise<void> {
+    await this.appService.createMessage(payload);
+    this.server.emit('recMessage', payload);
+  }
+
+  afterInit(server: any) {
+    console.log(server);
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`Connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected: ${client.id}`);
   }
 }
